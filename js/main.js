a218ce6a9a2f90339c8a1bf1fb7a3576bf32b979
ruthbriz <ruthbriz06@gmail.com>
2022-04-30 17:25:11 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ const links = [
     label: "Week1 notes",
     url: "week1/index.html"
   }
-  {
+  ,{
+    label: "W1Notes",
+    url: "/notes/index.html"
+  }
+  ,{
     label: "Week2 notes",
     url: "week2/index.html"
   }
